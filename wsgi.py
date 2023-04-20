dadc0997e295c33d573be94c46b0e5460236c341
Благодетелев Алексей Игоревич <blagodetelevai@Gidranet.local>
2023-04-20 13:17:24 +0500

    lesson_3

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,39 @@
 from blog.app import create_app
+from blog.models.database import db
 
 
-if __name__ == '__main__':
-    app = create_app() 
-    app.run(
-        host='0.0.0.0',
-        debug=True,
-    )
+
+app = create_app() 
+app.run(
+    host='0.0.0.0',
+    debug=True,
+)
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in terminal:
+    flask init-db
+    """
+
+    db.create_all()
+    print("DB initialized!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in terminal:
+    flask create-users
+    """
+
+    from blog.models import User
+    admin =  User(username="admin", is_staff=True)
+    james = User(username="james")
+
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+
+    print("Users created:", admin, james)
