727d058a7293f183df4c5eae2c481010ee3fb9bc
Rithman <darth-maul@mail.ru>
2023-04-26 09:27:56 +0500

    lesson_5

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import NotFound
 
 from blog.models.user import User
+from blog.models.database import db
+from blog.forms.user import ResgistrationForm, LoginForm
 
 
 auth_app = Blueprint("auth_app", __name__)
@@ -21,19 +25,30 @@ def unauthorized():
 
 @auth_app.route("/login/", methods=["GET", "POST"], endpoint="login")
 def login():
-    if request.method == "GET":
-        return render_template("auth/login.html")
-
-    username = request.form.get("username")
-    if not username:
-        render_template("auth/login.html", error="username not passed")
-
-    user = User.query.filter_by(username=username).one_or_none()
-    if user is None:
-        return render_template("auth/login.html", error=f"no user {username} found")
+    if current_user.is_authenticated:
+        return redirect("index.render_index")
     
-    login_user(user)
-    return redirect(url_for("index.render_index"))
+    form = LoginForm(request.form)
+
+    if request.method == "POST" and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).one_or_none()
+        if user is None:
+            return render_template("auth/login.html", form=form, error="username doesn't exist")
+        if not user.validate_password(form.password.data):
+            return render_template("auth/login.html", form=form, error="invalid username or password")
+        
+        login_user(user)
+        return redirect(url_for("index.render_index"))
+    
+    return render_template("auth/login.html", form=form)
+        
+
+
+@auth_app.route("/login_as/", methods=["GET", "POST"], endpoint="login_as")
+def login_as():
+    if not (current_user.is_authenticated and current_user.is_staff):
+        raise NotFound
+
 
 
 @auth_app.route("/logout/", endpoint="logout")
@@ -48,4 +63,40 @@ def logout():
 def secret_view():
     return "Secret data!"
 
-__all__ = ["login_manager", "auth_app"]
+
+@auth_app.route("/register/", methods=["GET", "POST"], endpoint="register")
+def register():
+    if current_user.is_authenticated:
+        return redirect("index.render_index")
+    
+    error = None
+    form = ResgistrationForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        if User.query.filter_by(username=form.username.data).count():
+            form.username.errors.append("username already exists!")
+            return render_template("auth/register.html", form=form)
+        
+        if User.query.filter_by(email=form.email.data).count():
+            form.email.errors.append("email already exists!")
+            return render_template("auth/register.html", form=form)
+        
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            username=form.username.data,
+            email=form.email.data,
+            is_staff=False,
+        )
+        user.password = form.password.data
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception("Could not create user!")
+            error = "Could not create user!"
+        else:
+            current_app.logger.info(f"Created user {user}")
+            login_user(user)
+            return redirect(url_for("index.render_index"))
+    return render_template("auth/register.html", form=form, error=error)
+
