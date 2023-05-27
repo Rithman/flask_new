ff4a8ed3da1ee0b974146c7e55357b843f840523
Rithman <darth-maul@mail.ru>
2023-05-27 08:53:12 +0500

    app.run fix

--- wsgi.py
+++ wsgi.py
@@ -6,39 +6,42 @@ from blog.models.database import db
 
 load_dotenv()
 
+
 app = create_app() 
-app.run(
-    host='0.0.0.0',
-    debug=True,
-)
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        debug=True,
+    )
 
 
-@app.cli.command("create-admin")
-def create_admin():
-    """
-    Run in terminal:
-    flask create-admin
-    """
+    @app.cli.command("create-admin")
+    def create_admin():
+        """
+        Run in terminal:
+        flask create-admin
+        """
 
-    from blog.models import User
-    admin =  User(username="admin", is_staff=True)
-    admin.password = os.environ.get("ADMIN_PASSWORD") or "adminpass"
+        from blog.models import User
+        admin =  User(username="admin", is_staff=True)
+        admin.password = os.environ.get("ADMIN_PASSWORD") or "adminpass"
 
-    db.session.add(admin)
-    db.session.commit()
+        db.session.add(admin)
+        db.session.commit()
 
-    print("Users created:", admin)
+        print("Users created:", admin)
 
 
-@app.cli.command("create-tags")
-def create_tags():
-    """
-    Run in terminal
-    → flask create-tags
-    """
-    from blog.models import Tag
-    for name in ["flask", "dkango", "python", "sqlalchemy", "news", "wtf", "omg"]:
-        tag = Tag(name=name)
-        db.session.add(tag)
-    db.session.commit()
-    print("Tags created!")
+    @app.cli.command("create-tags")
+    def create_tags():
+        """
+        Run in terminal
+        → flask create-tags
+        """
+        from blog.models import Tag
+        for name in ["flask", "dkango", "python", "sqlalchemy", "news", "wtf", "omg"]:
+            tag = Tag(name=name)
+            db.session.add(tag)
+        db.session.commit()
+        print("Tags created!")
